d58883dad869ca2e8f78c8b58bc3aea82bf0dee6
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-07-29 01:03:19 +0300

    Add files via upload

---
+++ 3.Ветвления/declension of the word ruble.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace declension_of_the_word_ruble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
