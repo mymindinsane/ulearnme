12d66586dc933f314f2d8107c44fe9b63a34752f
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-07-29 20:30:43 +0300

    5 part of the course

---
+++ 5.Массивы/even array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace even_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+        public static int[] GetFirstEvenNumbers(int amount)
+        {
+            int[] array = new int[amount];
+            for (int i = 0; i < array.Length+1; i++)
+            {
+                array[i] = (i+1) * 2;
+            }
+            return array;
+        }   
+
+    }
+}
---
+++ 5.Массивы/high index.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace High_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        
+        public static int MaxIndex(double[] array)
+        {	
+            if (array.Length==0)
+                return -1;
+            return Array.IndexOf(array,array.Max());
+        }
+    }
+}
