433d291d44f1e90a54beaf9daaf5fb789aba4ea9
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-06 23:32:12 +0300

    Update declension of the word ruble.cs

--- 3.Ветвления/declension of the word ruble.cs
+++ 3.Ветвления/declension of the word ruble.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace declension_of_the_word_ruble
+namespace Pluralize
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			
+			if ((count % 10 == 0) ||(count %10 >= 5) || ((count% 100>=10) && (count%100 <=20)))
+				return "рублей";
+			else if (count % 10 == 1)
+				return "рубль";
+			else
+				return "рубля";
+
+
+		}
+	}
 }
+
