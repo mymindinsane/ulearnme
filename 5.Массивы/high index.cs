0a356fee7f5afb60e1731fdb88dcb1dab5814e0e
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-10 06:51:40 +0300

    Update high index.cs

--- 5.Массивы/high index.cs
+++ 5.Массивы/high index.cs
@@ -1,20 +1,19 @@
-﻿using System;
+//Находим индекс максимального элемента
+
+using System;
 using System.Linq;
 
-namespace High_Index
+public class HelloWorld
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-           
-        }
-        
-        public static int MaxIndex(double[] array)
+       double[] arr = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; 
+		Console.WriteLine(Convert.ToString(MaxIndex(arr)));
+    }
+    public static int MaxIndex(double[] array)
         {	
             if (array.Length==0)
                 return -1;
             return Array.IndexOf(array,array.Max());
         }
-    }
 }
