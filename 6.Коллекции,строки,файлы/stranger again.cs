84d5cb159bf38646a8f5ea3e361f6d3c830bebb9
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-13 03:27:02 +0300

    Add files via upload

---
+++ 6.Коллекции,строки,файлы/stranger again.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace stranger_again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = new string[] { "push Привет! Это снова я! Пока!",
+                "pop 5",
+                "push Как твои успехи? Плохо?",
+                "push qwertyuiop",
+                "push 1234567890",
+                "pop 26" };
+
+            Console.WriteLine($"\"{ApplyCommands(commands)}\"");
+        }
+
+        public string ApplyCommands(string[] commands)
+        {
+            var builder = new StringBuilder();
+        }
+    }
+}
