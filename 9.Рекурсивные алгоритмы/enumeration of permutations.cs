7c29cc8c9fa32d2419779c0e3bd78d06c6ac6e6e
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-18 02:10:07 +0300

    Update enumeration of permutations.cs

--- 9.Рекурсивные алгоритмы/enumeration of permutations.cs
+++ 9.Рекурсивные алгоритмы/enumeration of permutations.cs
@@ -1,46 +1,52 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
-
-static void Main()
+namespace enumeration_of_permutations
 {
-    TestOnSize(1);
-    TestOnSize(2);
-    TestOnSize(0);
-    TestOnSize(3);
-    TestOnSize(4);
-}
-
-    static void TestOnSize(int size)
-{
-    var result = new List<int[]>();
-    MakePermutations(new int[size], 0, result);
-    foreach (var permutation in result)
-        WritePermutation(permutation);
-}
-
-    static void WritePermutation(int[] permutation)
-{
-    var strings = new List<string>();
-    foreach (var i in permutation)
-        strings.Add(i.ToString());
-    Console.WriteLine(string.Join(" ", strings.ToArray()));
-}
-    static void MakePermutations(int[] permutation, int position, List<int[]> result)
-{
-    if (position == permutation.Length)
+    class Program
     {
-        WritePermutation(permutation);
-        return;
-    }
-    else
-    {
-        for (int i = 0; i < permutation.Length; i++)
+        static void Main()
         {
-            var index = Array.IndexOf(permutation, i, 0, position);
-            if (index == -1)
+            TestOnSize(1);
+            TestOnSize(2);
+            TestOnSize(0);
+            TestOnSize(3);
+            TestOnSize(4);
+        }
+
+        static void TestOnSize(int size)
+        {
+            var result = new List<int[]>();
+            MakePermutations(new int[size], 0, result);
+            foreach (var permutation in result)
+                WritePermutation(permutation);
+        }
+
+        static void WritePermutation(int[] permutation)
+        {
+            var strings = new List<string>();
+            foreach (var i in permutation)
+                strings.Add(i.ToString());
+            Console.WriteLine(string.Join(" ", strings.ToArray()));
+        }
+        static void MakePermutations(int[] permutation, int position, List<int[]> result)
+        {
+            if (position == permutation.Length)
             {
-                permutation[position] = i;
-                MakePermutations(permutation, position+1, result);
+                WritePermutation(permutation);
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < permutation.Length; i++)
+                {
+                    var index = Array.IndexOf(permutation, i, 0, position);
+                    if (index == -1)
+                    {
+                        permutation[position] = i;
+                        MakePermutations(permutation, position+1, result);
+                    }
+                }
             }
         }
     }
