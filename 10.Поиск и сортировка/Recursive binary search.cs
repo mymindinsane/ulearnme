45b2043d9d8e9e01cd7493673e52f33f3ff2295f
mymindinsane <33089089+mymindinsane@users.noreply.github.com>
2022-08-23 00:26:26 +0300

    Update Recursive binary search.cs

--- 10.Поиск и сортировка/Recursive binary search.cs
+++ 10.Поиск и сортировка/Recursive binary search.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Recursive_binary_search
 {
@@ -6,31 +6,32 @@ namespace Recursive_binary_search
     {
         static void Main(string[] args)
         {
-            long[] arr = { 2, 4, 4, 6 };
+            long[] arr = { -1, 2, 4, 4, 6, 8, 11, 11, 16, 20, 22 };
             int result;
-            long value;
-            for (value = 1; value < 8; value++)
+            long desiredNumber;
+            for (desiredNumber = 1; desiredNumber < 23; desiredNumber++)
             { 
-                result = FindLeftBorder(arr, value);
-                Console.WriteLine("{0} {1}",(int)value, result);
+                result = FindLeftBorder(arr, desiredNumber);
+                Console.WriteLine("{0} {1}",(int)desiredNumber, result);
             }
             long[] array = new long[] { };
-            value = 1;
+            desiredNumber = 1;
             result = FindLeftBorder(array, 1);
-            Console.WriteLine("{0} {1}", (int)value, result);
+            Console.WriteLine("{0} {1}", (int)desiredNumber, result);
             Console.ReadKey();
         }
+        
         private static int FindLeftBorder(long[] arr, long value)
         {
             return BinSearchLeftBorder(arr, value, -1, arr.Length);
         }
-        //public static int BinSearchLeftBorder(long[] array, long value, int left, int right)
+        public static int BinSearchLeftBorder(long[] array, long desiredNumber, int left, int right)
         {
-            if (...) return left;
+            if (right - left == 1) return left;//поиск левой границы максимального элемента меньшего чем desiredNumber
             var m = (left + right) / 2;
-            if (array[m] < value)
-                return BinSearchLeftBorder(array, value, ...);
-            return BinSearchLeftBorder(array, value, ...);
+            if (array[m] < desiredNumber)
+                return BinSearchLeftBorder(array, desiredNumber, m, right );
+            return BinSearchLeftBorder(array, desiredNumber, left, m);
         }
     }
 }
